caf4914bcedf41a1072e767c60bc8406d01fd242
Hassan <hakhan0301@gmail.com>
2022-06-18 16:56:18 -0500

    tailwind setup

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="bg-orange-500">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
 
     </div>
   );
