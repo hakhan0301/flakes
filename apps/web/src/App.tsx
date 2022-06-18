abe02f393e6e94a76c110ba31ea158a825be2234
isaiahmbernal <39536392+isaiahmbernal@users.noreply.github.com>
2022-06-18 18:13:23 -0500

    Added form

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,28 @@
+import React from "react";
+import { useState } from "react";
+
 interface Cron {
   title: string;
   cronTime: string;
   URL: string;
 }
 
-const cronJobs: Cron[] = [
-  { title: 'Daily', cronTime: '0 0 0 * * *', URL: 'http://localhost:3000/daily' },
-  { title: 'Weekly', cronTime: '0 0 0 * * 0', URL: 'http://localhost:3000/weekly' },
-  { title: 'Monthly', cronTime: '0 0 0 1 * *', URL: 'http://localhost:3000/monthly' },
+const initialCronJobs: Cron[] = [
+  {
+    title: "Daily",
+    cronTime: "0 0 0 * * *",
+    URL: "http://localhost:3000/daily",
+  },
+  {
+    title: "Weekly",
+    cronTime: "0 0 0 * * 0",
+    URL: "http://localhost:3000/weekly",
+  },
+  {
+    title: "Monthly",
+    cronTime: "0 0 0 1 * *",
+    URL: "http://localhost:3000/monthly",
+  },
 ];
 
 function CronJob(props: Cron) {
@@ -22,25 +37,57 @@ function CronJob(props: Cron) {
   );
 }
 
-
 function App() {
+  const [cronJobs, setCronJobs] = useState<Cron[]>(initialCronJobs);
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [url, setUrl] = useState("");
+
+  function submitCronJob(event: React.FormEvent) {
+    event.preventDefault();
+    const cron: Cron = { title: title, cronTime: time, URL: url };
+    setCronJobs([...cronJobs, cron]);
+  }
+
   return (
     <div className="bg-orange-500 h-screen w-screen">
       <div className="flex flex-col">
         {/* navbar */}
         <div className="bg-sky-400 p-4 text-emerald-800">
-          <h1 className="text-3xl font-bold">
-            Cereal - CRON Server
-          </h1>
+          <h1 className="text-3xl font-bold">Cereal - CRON Server</h1>
         </div>
+
+        <form className="flex flex-col items-center" onSubmit={submitCronJob}>
+          <label>Job Title:</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          ></input>
+          <label>Job Time:</label>
+          <input
+            type="text"
+            value={time}
+            onChange={(event) => setTime(event.target.value)}
+          ></input>
+          <label>Job URL:</label>
+          <input
+            type="text"
+            value={url}
+            onChange={(event) => setUrl(event.target.value)}
+          ></input>
+          <button type="submit" className="bg-white">
+            Submit
+          </button>
+        </form>
 
         <div className="w-full flex flex-col items-center gap-4 p-4">
           {/* cron jobs */}
-          {cronJobs.map((job) => <CronJob {...job} />)}
+          {cronJobs.map((job) => (
+            <CronJob {...job} />
+          ))}
         </div>
-
       </div>
-
     </div>
   );
 }
