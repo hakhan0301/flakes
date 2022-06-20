2a8cca2d145964b9bca50b6d29895c75f98cfeb7
Hassan <hakhan0301@gmail.com>
2022-06-20 17:09:09 -0500

    sussy colors

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -18,35 +18,35 @@ interface CronJobProps extends CronJob {
 }
 
 const bgColors = [
-  'bg-rose-400/[1.0]',
-  'bg-rose-400/[.95]',
-  'bg-rose-400/[.90]',
-  'bg-rose-400/[.85]',
-  'bg-rose-400/[.80]',
-  'bg-rose-400/[.75]',
-  'bg-rose-400/[.70]',
-  'bg-rose-400/[.65]',
-  'bg-rose-400/[.60]',
-  'bg-rose-400/[.55]',
-  'bg-rose-400/[.50]',
-  'bg-rose-400/[.45]',
-  'bg-rose-400/[.40]',
-  'bg-rose-400/[.35]',
-  'bg-rose-400/[.30]',
-  'bg-rose-400/[.25]',
-  'bg-rose-400/[.20]',
-  'bg-rose-400/[.15]',
-  'bg-rose-400/[.10]',
-  'bg-rose-400/[.5]',
+  'bg-yellow-200/[1.0] text-orange-700',
+  'bg-yellow-200/[.95] text-orange-700',
+  'bg-yellow-200/[.90] text-orange-700',
+  'bg-yellow-200/[.85] text-orange-700',
+  'bg-yellow-200/[.80] text-orange-700',
+  'bg-yellow-200/[.75] text-orange-700',
+  'bg-yellow-200/[.70] text-orange-800',
+  'bg-yellow-200/[.65] text-orange-800',
+  'bg-yellow-200/[.60] text-orange-800',
+  'bg-yellow-200/[.55] text-orange-800',
+  'bg-yellow-200/[.50] text-orange-800',
+  'bg-yellow-200/[.45] text-orange-800',
+  'bg-yellow-200/[.40] text-orange-800',
+  'bg-yellow-200/[.35] text-orange-900',
+  'bg-yellow-200/[.30] text-orange-900',
+  'bg-yellow-200/[.25] text-orange-900',
+  'bg-yellow-200/[.20] text-orange-900',
+  'bg-yellow-200/[.15] text-orange-900',
+  'bg-yellow-200/[.10] text-orange-900',
+  'bg-yellow-200/[.5]  text-orange-900',
 ]
 
 function CronJob({ title, cron, url, index, onDelete }: CronJobProps) {
-  const bgOpacity = Math.floor(index * bgColors.length);
+  const bgOpacity = Math.floor((index) * bgColors.length);
 
   return (
     <tr className={`table-row ${bgColors[bgOpacity]}`}>
       <td>
-        <button onClick={() => onDelete(title)} className='table-cell py-1 pl-3 hover:text-teal-300'>
+        <button onClick={() => onDelete(title)} className='table-cell py-1 pl-3 hover:text-purple-600'>
           <IoMdTrash />
         </button>
       </td>
@@ -59,9 +59,9 @@ function CronJob({ title, cron, url, index, onDelete }: CronJobProps) {
 
 function CronForm() {
   return (
-    <tr>
+    <tr className=''>
       <td className='w-full h-full group cursor-pointer'>
-        <FaPlus className='group-hover:text-teal-300 mx-auto' />
+        <FaPlus className='group-hover:text-purple-600 mx-auto' />
       </td>
       <td><p className='py-1 pr-2'>title</p></td>
       <td><p className='py-1 px-2'>cron</p></td>
@@ -74,7 +74,7 @@ interface Props {
   jobs: CronJob[]
 };
 const Home: NextPage<Props> = ({ jobs: _jobs }) => {
-  const [jobs, setJobs] = useState<CronJob[]>(_jobs);
+  const [jobs, setJobs] = useState<CronJob[]>(_jobs.flatMap(job => [job, job, job, job, job, job]));
 
   const removeJob = async (title: string) => {
     await fetch('/api/jobs/delete', {
@@ -86,9 +86,9 @@ const Home: NextPage<Props> = ({ jobs: _jobs }) => {
 
 
   return (
-    <div className='min-h-screen bg-rose-500 text-gray-900'>
+    <div className='min-h-screen bg-gradient-to-b from-yellow-300 to-red-400 text-orange-900'>
       {/* navbar */}
-      <nav className='bg-sky-400 shadow-lg'>
+      <nav className='bg-red-500 shadow-lg'>
         <div className='flex md:flex-row items-center p-6'>
           <div className='flex flex-row gap-4 items-center'>
             <p className='text-3xl font-bold font-mono'>
@@ -107,8 +107,8 @@ const Home: NextPage<Props> = ({ jobs: _jobs }) => {
 
       {/* cron table */}
       <div className='flex flex-col gap-3 p-12'>
-        <table className='table-fixed w-full shadow-md'>
-          <thead className='bg-sky-300 font-bold text-gray-700'>
+        <table className='table-fixed w-full'>
+          <thead className='bg-red-500 font-bold'>
             <tr>
               <th className="text-left w-10"></th>
               <th className="text-left pr-3 py-2">Title</th>
