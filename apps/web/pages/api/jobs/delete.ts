3c6123071a0f899c6795df25f335ab081b922f10
Hassan <hakhan0301@gmail.com>
2022-06-19 23:05:32 -0500

    delete job

---
+++ apps/web/pages/api/jobs/delete.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+// @ts-ignore
+import cronJoi from 'joi-cron-expression';
+import _Joi from 'joi';
+import { endointWrapper, validator } from "@cereal/api-helpers";
+import { prisma } from '@cereal/db';
+
+const Joi = cronJoi(_Joi);
+
+const bodySchema = Joi.object({
+  title: Joi.string().min(5).required()
+});
+
+export default endointWrapper()
+  .use(validator(
+    { body: bodySchema }
+  ))
+  .post(handler);
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const body = req.body;
+  await prisma.cronJob.delete({
+    where: { title: body.title }
+  })
+  res.json('Success');
+}
