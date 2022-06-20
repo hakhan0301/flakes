73190c1d4d3ecb6b573d34ae239c1ce454b1b9f2
Hassan <hakhan0301@gmail.com>
2022-06-20 17:44:20 -0500

    next config cron server

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,6 +4,7 @@ const withPlugins = require('next-compose-plugins');
 const withTM = require("next-transpile-modules")([
   "@flakes/api-helpers",
   "@flakes/db",
+  "@flakes/cron-server"
 ]);
 
 module.exports = withPlugins([
