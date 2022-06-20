7d3d0ec3a4656d2659a315e40c20e36b86ea0c66
Hassan <hakhan0301@gmail.com>
2022-06-20 16:38:24 -0500

    design + gradient

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -8,43 +8,65 @@ import { useState } from 'react'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const jobs = await prisma.cronJob.findMany();
-
-  return {
-    props: {
-      jobs
-    }
-  }
+  return { props: { jobs } };
 }
+
 
 interface CronJobProps extends CronJob {
   index: number
   onDelete: (title: string) => void
 }
+
+const bgColors = [
+  'bg-rose-400/[1.0]',
+  'bg-rose-400/[.95]',
+  'bg-rose-400/[.90]',
+  'bg-rose-400/[.85]',
+  'bg-rose-400/[.80]',
+  'bg-rose-400/[.75]',
+  'bg-rose-400/[.70]',
+  'bg-rose-400/[.65]',
+  'bg-rose-400/[.60]',
+  'bg-rose-400/[.55]',
+  'bg-rose-400/[.50]',
+  'bg-rose-400/[.45]',
+  'bg-rose-400/[.40]',
+  'bg-rose-400/[.35]',
+  'bg-rose-400/[.30]',
+  'bg-rose-400/[.25]',
+  'bg-rose-400/[.20]',
+  'bg-rose-400/[.15]',
+  'bg-rose-400/[.10]',
+  'bg-rose-400/[.5]',
+]
+
 function CronJob({ title, cron, url, index, onDelete }: CronJobProps) {
+  const bgOpacity = Math.floor(index * bgColors.length);
+
   return (
-    <div className={`table-row ${index % 2 == 0 ? 'bg-rose-300' : 'bg-rose-400'}`}>
-      <button onClick={() => onDelete(title)}
-        className='table-cell py-1 pl-3 hover:text-teal-300'
-      >
-        <IoMdTrash />
-      </button>
-      <p className='table-cell py-1 pr-2'>{title}</p>
-      <p className='table-cell py-1 px-2'>{cron}</p>
-      <p className='table-cell py-1 px-2'>{url}</p>
-    </div>
+    <tr className={`table-row ${bgColors[bgOpacity]}`}>
+      <td>
+        <button onClick={() => onDelete(title)} className='table-cell py-1 pl-3 hover:text-teal-300'>
+          <IoMdTrash />
+        </button>
+      </td>
+      <td><p className='table-cell py-1 pr-2'>{bgOpacity}{title}</p></td>
+      <td><p className='table-cell py-1 px-2'>{cron}</p></td>
+      <td><p className='table-cell py-1 px-2'>{url}</p></td>
+    </tr>
   );
 }
 
 function CronForm() {
   return (
-    <div className='table-row'>
-      <button className='table-cell w-1 py-1 pl-3 hover:text-teal-300' >
-        <FaPlus />
-      </button>
-      <p className='table-cell py-1 pr-2'>title</p>
-      <p className='table-cell py-1 px-2'>cron</p>
-      <p className='table-cell py-1 px-2'>url</p>
-    </div >
+    <tr>
+      <td className='w-full h-full group cursor-pointer'>
+        <FaPlus className='group-hover:text-teal-300 mx-auto' />
+      </td>
+      <td><p className='py-1 pr-2'>title</p></td>
+      <td><p className='py-1 px-2'>cron</p></td>
+      <td><p className='py-1 px-2'>url</p></td>
+    </tr >
   )
 }
 
@@ -52,7 +74,6 @@ interface Props {
   jobs: CronJob[]
 };
 const Home: NextPage<Props> = ({ jobs: _jobs }) => {
-  // jobs state
   const [jobs, setJobs] = useState<CronJob[]>(_jobs);
 
   const removeJob = async (title: string) => {
@@ -65,10 +86,9 @@ const Home: NextPage<Props> = ({ jobs: _jobs }) => {
 
 
   return (
-
     <div className='min-h-screen bg-rose-500 text-gray-900'>
       {/* navbar */}
-      <nav className='bg-sky-300 shadow-lg'>
+      <nav className='bg-sky-400 shadow-lg'>
         <div className='flex md:flex-row items-center p-6'>
           <div className='flex flex-row gap-4 items-center'>
             <p className='text-3xl font-bold font-mono'>
@@ -85,22 +105,24 @@ const Home: NextPage<Props> = ({ jobs: _jobs }) => {
         </div>
       </nav>
 
-
-      <div className='flex flex-col gap-3 p-6'>
-        <div className='table w-full'>
-          <div className='table-header-group bg-pink-200 font-bold text-gray-700'>
-            <div className="table-cell text-left py-2"></div>
-            <div className="table-cell text-left pr-3 py-2">Title</div>
-            <div className="table-cell text-left px-2 py-2">CRON</div>
-            <div className="table-cell text-left px-2 py-2">Endpoint</div>
-
-          </div>
-          <div className="table-row-group">
-            {jobs.map((job, i) => <CronJob key={job.title} index={i}
-              onDelete={removeJob} {...job} />)}
+      {/* cron table */}
+      <div className='flex flex-col gap-3 p-12'>
+        <table className='table-fixed w-full shadow-md'>
+          <thead className='bg-sky-300 font-bold text-gray-700'>
+            <tr>
+              <th className="text-left w-10"></th>
+              <th className="text-left pr-3 py-2">Title</th>
+              <th className="text-left px-2 py-2">CRON</th>
+              <th className="text-left px-2 py-2">Endpoint</th>
+            </tr>
+          </thead>
+          <tbody>
+            {jobs.map((job, i) =>
+              <CronJob key={job.title} index={i / jobs.length}
+                onDelete={removeJob} {...job} />)}
             < CronForm />
-          </div>
-        </div>
+          </tbody>
+        </table>
       </div>
     </div>
   )
